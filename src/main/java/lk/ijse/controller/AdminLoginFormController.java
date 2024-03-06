2956781b2650d65aa7bb1d6c7dee6a388450bdb5
yehani <yehanipamunuwa@gmail.com>
2024-03-06 14:13:46 +0530

    Create AdminLogin_Form.fxml

--- src/main/java/lk/ijse/controller/AdminLoginFormController.java
+++ src/main/java/lk/ijse/controller/AdminLoginFormController.java
@@ -1,4 +1,32 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class AdminLoginFormController {
+
+    @FXML
+    private AnchorPane adminRoot;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtServiceNumber;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void hyperSignUpOnAction(ActionEvent event) {
+
+    }
+
 }
