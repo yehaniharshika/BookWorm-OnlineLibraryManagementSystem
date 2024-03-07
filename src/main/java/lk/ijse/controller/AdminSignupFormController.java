522419524afac9c6c779ad15609c65e64c9f2592
yehani <yehanipamunuwa@gmail.com>
2024-03-07 16:58:04 +0530

    create AdminSignupFormController

---
+++ src/main/java/lk/ijse/controller/AdminSignupFormController.java
@@ -0,0 +1,65 @@
+package lk.ijse.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class AdminSignupFormController {
+
+    @FXML
+    private JFXButton btnCreateAccount;
+
+    @FXML
+    private JFXButton btnDeleteAccount;
+
+    @FXML
+    private JFXButton btnUpdateAccount;
+
+    @FXML
+    private TextField txtAdminID;
+
+    @FXML
+    private TextField txtEmailAddress;
+
+    @FXML
+    private TextField txtFirstname;
+
+    @FXML
+    private TextField txtLastname;
+
+    @FXML
+    private TextField txtNIC;
+
+    @FXML
+    private TextField txtPassword;
+
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnCreateAccountOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteAccountOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateAccountOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void hyperLoginHereOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txtAuthorIDOnAction(ActionEvent event) {
+
+    }
+
+}
