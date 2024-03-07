42c5d2fed84db26d028824fe09602ccacd08bdac
yehani <yehanipamunuwa@gmail.com>
2024-03-07 16:58:18 +0530

    create AdminDashboard

--- src/main/java/lk/ijse/controller/DashboardFormController.java
+++ src/main/java/lk/ijse/controller/DashboardFormController.java
@@ -1,5 +1,46 @@
 package lk.ijse.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class DashboardFormController {
+
+    @FXML
+    private AnchorPane MainRoot;
+
+    @FXML
+    private AnchorPane Root;
+
+    @FXML
+    private Label lblAuthorCount;
+
+    @FXML
+    private Label lblBookCount;
+
+    @FXML
+    private Label lblBorrowCount;
+
+    @FXML
+    private Label lblMemberCount;
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUserInformationOnAction(ActionEvent event) throws IOException {
+        Parent node = FXMLLoader.load(this.getClass().getResource("/view/User_Form.fxml"));
+
+        this.Root.getChildren().clear();
+        this.Root.getChildren().add(node);
+
+    }
 
 }
