e1165ffbda155260fe02c0f7dfc7c4fb2dfe23f8
yehani <yehanipamunuwa@gmail.com>
2024-03-10 13:22:29 +0530

    Create library branch

---
+++ src/main/java/lk/ijse/dao/LibraryBranchDAOImpl.java
@@ -0,0 +1,42 @@
+package lk.ijse.dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class LibraryBranchDAOImpl implements LibraryBranchDAO{
+
+    @Override
+    public String getCount() throws SQLException {
+        return "";
+    }
+
+    @Override
+    public String generateNextId() throws SQLException {
+        return "";
+    }
+
+    @Override
+    public boolean save(Object dto) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean update(Object dto) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public boolean delete(String id) throws SQLException {
+        return false;
+    }
+
+    @Override
+    public Object search(String id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public ArrayList getAll() throws SQLException {
+        return null;
+    }
+}
