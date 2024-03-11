aaaebf9b0b0e89771618550465cf15fd64f43270
yehani <yehanipamunuwa@gmail.com>
2024-03-11 09:56:56 +0530

    implement LibraryBranchBO and LibraryBranchBOImpl

--- src/main/java/lk/ijse/bo/LibraryBranchBO.java
+++ src/main/java/lk/ijse/bo/LibraryBranchBO.java
@@ -1,4 +1,20 @@
 package lk.ijse.bo;
 
+import lk.ijse.dto.LibraryBranchDTO;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public interface LibraryBranchBO {
+    String generateNextBranchId() throws SQLException;
+
+    boolean saveLibraryBranch(LibraryBranchDTO dto) throws SQLException;
+
+    boolean updateLibraryBranch(LibraryBranchDTO dto) throws SQLException;
+
+    boolean deleteLibraryBranch(String branchID) throws SQLException;
+
+    LibraryBranchDTO searchLibraryBranch(String branchID) throws SQLException;
+
+    ArrayList<LibraryBranchDTO> getAllLibraryBranches() throws SQLException;
 }
--- src/main/java/lk/ijse/bo/LibraryBranchBOImpl.java
+++ src/main/java/lk/ijse/bo/LibraryBranchBOImpl.java
@@ -1,5 +1,76 @@
 package lk.ijse.bo;
 
-public class LibraryBranchBOImpl {
+import lk.ijse.dao.LibraryBranchDAOImpl;
+import lk.ijse.dto.LibraryBranchDTO;
+import lk.ijse.entity.LibraryBranch;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LibraryBranchBOImpl implements LibraryBranchBO{
+
+    public LibraryBranchDAOImpl libraryBranchDAO = new LibraryBranchDAOImpl();
+
+    @Override
+    public String generateNextBranchId() throws SQLException {
+        return libraryBranchDAO.generateNextId();
+    }
+
+    @Override
+    public boolean saveLibraryBranch(LibraryBranchDTO dto) throws SQLException {
+        return libraryBranchDAO.save(new LibraryBranch(
+                dto.getBranchID(),
+                dto.getBranchName(),
+                dto.getLocation(),
+                dto.getDescription(),
+                dto.getAdminID()
+        ));
+    }
+
+    @Override
+    public boolean updateLibraryBranch(LibraryBranchDTO dto) throws SQLException {
+        return libraryBranchDAO.update(new LibraryBranch(
+                dto.getBranchID(),
+                dto.getBranchName(),
+                dto.getLocation(),
+                dto.getDescription(),
+                dto.getAdminID()
+        ));
+    }
+
+    @Override
+    public boolean deleteLibraryBranch(String branchID) throws SQLException {
+        return libraryBranchDAO.delete(branchID);
+    }
+
+    @Override
+    public LibraryBranchDTO searchLibraryBranch(String branchID) throws SQLException {
+
+        LibraryBranch libraryBranch = libraryBranchDAO.search(branchID);
+
+        if (libraryBranch == null){
+            return new LibraryBranchDTO(libraryBranch);
+        }else {
+            return null;
+        }
+
+    }
+
+    @Override
+    public ArrayList<LibraryBranchDTO> getAllLibraryBranches() throws SQLException {
+        List<LibraryBranch> getAllLibraryBranches = libraryBranchDAO.getAll();
+        ArrayList<LibraryBranchDTO> libraryBranchDTOS = new ArrayList<>();
+        
+        for (LibraryBranch libraryBranch : getAllLibraryBranches){
+            libraryBranchDTOS.add(new LibraryBranchDTO(
+                    libraryBranch.getBranchID(),
+                    libraryBranch.getBranchName(),
+                    libraryBranch.getLocation(),
+                    libraryBranch.getDescription(),
+                    libraryBranch.getAdminID()
+            ));
+        }
+        return libraryBranchDTOS;
+    }
 }
