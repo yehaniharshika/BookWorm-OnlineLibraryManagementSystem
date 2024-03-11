8a6edd7051f974d59aec8906a3a7f04ddd227ad4
yehani <yehanipamunuwa@gmail.com>
2024-03-11 09:55:50 +0530

    create LibraryBranchTM class

--- src/main/java/lk/ijse/dto/tm/LibraryBranchTM.java
+++ src/main/java/lk/ijse/dto/tm/LibraryBranchTM.java
@@ -1,4 +1,16 @@
 package lk.ijse.dto.tm;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class LibraryBranchTM {
+    private String branchID;
+    private String branchName;
+    private String location;
+    private String description;
+    private String adminID;
 }
