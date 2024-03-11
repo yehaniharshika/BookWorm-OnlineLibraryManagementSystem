f036f0a146b71a4c1e297364c12d15accf0360b8
yehani <yehanipamunuwa@gmail.com>
2024-03-11 08:51:01 +0530

    Create BookList_Form.fxml and BookListFormController

---
+++ src/main/java/lk/ijse/controller/BookListFormController.java
@@ -0,0 +1,38 @@
+package lk.ijse.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+
+public class BookListFormController {
+
+    @FXML
+    private TableColumn<?, ?> colAuthorName;
+
+    @FXML
+    private TableColumn<?, ?> colAvailabilityStatus;
+
+    @FXML
+    private TableColumn<?, ?> colBookGenre;
+
+    @FXML
+    private TableColumn<?, ?> colBookId;
+
+    @FXML
+    private TableColumn<?, ?> colBookName;
+
+    @FXML
+    private TableColumn<?, ?> colBranchId;
+
+    @FXML
+    private TableColumn<?, ?> colQty;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private TableView<?> tblBookDetails;
+
+}
+
