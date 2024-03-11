e42b25d3b60d0679cc7057f4257cf1d3675f8f74
yehani <yehanipamunuwa@gmail.com>
2024-03-11 09:55:38 +0530

    create LibraryBranchDTO

--- src/main/java/lk/ijse/dto/LibraryBranchDTO.java
+++ src/main/java/lk/ijse/dto/LibraryBranchDTO.java
@@ -1,5 +1,26 @@
 package lk.ijse.dto;
 
-public class LibraryBranchDTO {
+import lk.ijse.entity.LibraryBranch;
+import lombok.*;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class LibraryBranchDTO {
+    private String branchID;
+    private String branchName;
+    private String location;
+    private String description;
+    private String adminID;
+
+
+    public LibraryBranchDTO(LibraryBranch libraryBranch) {
+        this.branchID = libraryBranch.getBranchID();
+        this.branchName = libraryBranch.getBranchName();
+        this.location = libraryBranch.getLocation();
+        this.description = libraryBranch.getDescription();
+        this.adminID = libraryBranch.getAdminID();
+    }
 }
